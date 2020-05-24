f60ae6c5659f7dcf2ac47a3de4e16e6eee961fbf
AllainPurrnal <llnbrnl017@gmail.com>
2020-05-23 21:35:10 -0700

    Updated Services in-project  wireframe

--- client/src/components/pages/Services/index.js
+++ client/src/components/pages/Services/index.js
@@ -6,123 +6,98 @@ const Services = () => {
   return (
     <div className="wrapper servicesWrap">
       <div className="service">
-        <img src="https://via.placeholder.com/200" />
-        <div className="serviceHeader">
-          <h1>Lorem 1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
-        </div>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
+        
+        <div className="serviceOption">
+          <div className="serviceHeader">
+            <h1>Lorem 1</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
+          </div>
 
-        <div className="serviceList">
+          <div className="serviceList">
           <ul>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-          </ul>
-        </div>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+            </ul>
+          </div>
 
-        <div className="btn">
-          <button>Book Now</button>
+          <div className="btn">
+            <button>Book Now</button>
+          </div>
         </div>
       </div>
 
       <div className="service">
-        <img src="https://via.placeholder.com/200" />
-        <div className="serviceHeader">
-          <h1>Lorem 1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
-        </div>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
+        
+        <div className="serviceOption">
+          <div className="serviceHeader">
+            <h1>Lorem 1</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
+          </div>
 
-        <div className="serviceList">
-          <ul>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-          </ul>
-        </div>
+          <div className="serviceList">
+            <ul>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+            </ul>
+          </div>
 
-        <div className="btn">
-          <button>Book Now</button>
-        </div>
-      </div>
-
-      <div className="service">
-        <img src="https://via.placeholder.com/200" />
-        <div className="serviceHeader">
-          <h1>Lorem 1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
-        </div>
-
-        <div className="serviceList">
-          <ul>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-          </ul>
-        </div>
-
-        <div className="btn">
-          <button>Book Now</button>
+          <div className="btn">
+            <button>Book Now</button>
+          </div>
         </div>
       </div>
 
       <div className="service">
-        <img src="https://via.placeholder.com/200" />
-        <div className="serviceHeader">
-          <h1>Lorem 1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
-        </div>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
+        
+        <div className="serviceOption">
+          <div className="serviceHeader">
+            <h1>Lorem 1</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
+          </div>
 
-        <div className="serviceList">
-          <ul>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-          </ul>
-        </div>
+          <div className="serviceList">
+            <ul>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+            </ul>
+          </div>
 
-        <div className="btn">
-          <button>Book Now</button>
+          <div className="btn">
+            <button>Book Now</button>
+          </div>
         </div>
       </div>
 
       <div className="service">
-        <img src="https://via.placeholder.com/200" />
-        <div className="serviceHeader">
-          <h1>Lorem 1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
-        </div>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
+        
+        <div className="serviceOption">
+          <div className="serviceHeader">
+            <h1>Lorem 1</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
+          </div>
 
-        <div className="serviceList">
+          <div className="serviceList">
           <ul>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-            <li>Duis eget tristique justo</li>
-          </ul>
-        </div>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+              <li>Duis eget tristique justo</li>
+            </ul>
+          </div>
 
-        <div className="btn">
-          <button>Book Now</button>
+          <div className="btn">
+            <button>Book Now</button>
+          </div>
         </div>
       </div>
 
