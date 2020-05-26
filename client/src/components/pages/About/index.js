6264ef904b3613bafcba1a93f03721aec7f554a0
AllainPurrnal <llnbrnl017@gmail.com>
2020-05-26 12:42:37 -0700

    About page wireframe completed. Now adding a development changelog to README

--- client/src/components/pages/About/index.js
+++ client/src/components/pages/About/index.js
@@ -5,7 +5,72 @@ const About = () => {
 
   return (
     <div className="wrapper aboutWrap">
-      <h1>About!</h1>
+
+      <div className="teamPicture">
+        <img src="https://via.placeholder.com/1435x350" alt="The Team" />
+      </div>
+
+      <div className="background">
+
+      </div>
+
+      <div className="meetTheTeam">
+        <div className="teamHeader">
+
+        </div>
+
+        <div className="teamBody">
+          <div className="person person1">
+            <img src="https://via.placeholder.com/200" alt="owner"/>
+            <h1>First Last</h1>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+
+          <div className="person person2">
+            <img src="https://via.placeholder.com/200" alt="owner"/>
+            <h1>First Last</h1>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+
+          <div className="person person3">
+            <img src="https://via.placeholder.com/150" alt="emp1"/>
+            <h1>First Last</h1>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+
+          <div className="person person4">
+            <img src="https://via.placeholder.com/150" alt="emp2"/>
+            <h1>First Last</h1>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+
+          <div className="person person5">
+            <img src="https://via.placeholder.com/150" alt="emp3"/>
+            <h1>First Last</h1>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+
+          <div className="person person6">
+            <img src="https://via.placeholder.com/150" alt="emp4"/>
+            <h1>First Last</h1>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
--- client/src/components/pages/Services/index.js
+++ client/src/components/pages/Services/index.js
@@ -77,28 +77,11 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="service">
+      <div className="addService">
         <img src="https://via.placeholder.com/250" alt="placeholder"/>
-        
-        <div className="serviceOption">
-          <div className="serviceHeader">
-            <h1>Lorem 1</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut enim nunc. Aenean ultrices sagittis faucibus.</p>
-          </div>
-
-          <div className="serviceList">
-          <ul>
-              <li>Duis eget tristique justo</li>
-              <li>Duis eget tristique justo</li>
-              <li>Duis eget tristique justo</li>
-              <li>Duis eget tristique justo</li>
-            </ul>
-          </div>
-
-          <div className="btn">
-            <button>Book Now</button>
-          </div>
-        </div>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
+        <img src="https://via.placeholder.com/250" alt="placeholder"/>
       </div>
 
     </div>
